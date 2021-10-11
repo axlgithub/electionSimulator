a205506c35ab3ff9b1a326eb704468518390cbbe
lubba <hyacinthe.chaigne@gmail.com>
2021-10-11 17:56:14 +0200

    second init

--- src/com/isne/Main.java
+++ src/com/isne/Main.java
@@ -3,6 +3,9 @@ package com.isne;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Intellij is better");
+        System.out.println("Si le projet n'est pas reconnu : File > Project structure > modules");
+        System.out.println("puis clic droit sur 'src' et 'source', ensuite recompiler et lancer Main");
+        System.out.println("Ou alors clic droit sur src puis 'mark as source directory'");
+        System.out.println("Pas ouf l'initialisation du repo...");
     }
 }
